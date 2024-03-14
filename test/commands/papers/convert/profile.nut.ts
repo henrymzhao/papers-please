f155dcc90863743e6c5dd1dc81de573397c9ba40
Henry Zhao <henry.zhao@slalom.com>
2024-03-14 15:03:27 -0700

    chore: fix test files

---
+++ test/commands/papers/convert/profile.nut.ts
@@ -0,0 +1,20 @@
+// import { execCmd, TestSession } from '@salesforce/cli-plugins-testkit';
+// import { expect } from 'chai';
+// import { ConvertProfileResult } from '../../../../src/commands/papers/convert/profile.js';
+//
+// let testSession: TestSession;
+//
+// describe('hello world NUTs', () => {
+//   before('prepare session', async () => {
+//     testSession = await TestSession.create();
+//   });
+//
+//   after(async () => {
+//     await testSession?.clean();
+//   });
+//
+//   // it('should say hello to the world', () => {
+//   //   const result = execCmd<HelloWorldResult>('hello world --json', { ensureExitCode: 0 }).jsonOutput?.result;
+//   //   expect(result?.name).to.equal('World');
+//   // });
+// });
---
+++ test/commands/papers/convert/profile.test.ts
@@ -0,0 +1,26 @@
+// import {TestContext} from '@salesforce/core/lib/testSetup.js';
+// import {expect} from 'chai';
+// import {stubSfCommandUx} from '@salesforce/sf-plugins-core';
+// import ConvertProfile from '../../../../src/commands/papers/convert/profile.js';
+//
+// describe('hello world', () => {
+//     const $$ = new TestContext();
+//     let sfCommandStubs: ReturnType<typeof stubSfCommandUx>;
+//
+//     beforeEach(() => {
+//         sfCommandStubs = stubSfCommandUx($$.SANDBOX);
+//     });
+//
+//     afterEach(() => {
+//         $$.restore();
+//     });
+//     //
+//     // it('runs hello world', async () => {
+//     //     await World.run([]);
+//     //     const output = sfCommandStubs.log
+//     //         .getCalls()
+//     //         .flatMap((c) => c.args)
+//     //         .join('\n');
+//     //     expect(output).to.include('Hello World');
+//     // });
+// });
