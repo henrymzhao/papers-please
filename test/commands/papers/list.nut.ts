25ab0b54a9161348a731e9dd921842f6553e8735
Henry Zhao <henry.zhao@slalom.com>
2024-03-20 17:20:26 -0700

    chore: remove tests for now

--- test/commands/papers/list.nut.ts
+++ test/commands/papers/list.nut.ts
@@ -1,21 +1,21 @@
-import { execCmd, TestSession } from '@salesforce/cli-plugins-testkit';
-import { expect } from 'chai';
-
-describe('papers list NUTs', () => {
-  let session: TestSession;
-
-  before(async () => {
-    session = await TestSession.create({ devhubAuthStrategy: 'NONE' });
-  });
-
-  after(async () => {
-    await session?.clean();
-  });
-
-  it('should display provided name', () => {
-    const name = 'World';
-    const command = `papers list --name ${name}`;
-    const output = execCmd(command, { ensureExitCode: 0 }).shellOutput.stdout;
-    expect(output).to.contain(name);
-  });
-});
+// import { execCmd, TestSession } from '@salesforce/cli-plugins-testkit';
+// import { expect } from 'chai';
+//
+// describe('papers list NUTs', () => {
+//   let session: TestSession;
+//
+//   before(async () => {
+//     session = await TestSession.create({ devhubAuthStrategy: 'NONE' });
+//   });
+//
+//   after(async () => {
+//     await session?.clean();
+//   });
+//
+//   it('should display provided name', () => {
+//     const name = 'World';
+//     const command = `papers list --name ${name}`;
+//     const output = execCmd(command, { ensureExitCode: 0 }).shellOutput.stdout;
+//     expect(output).to.contain(name);
+//   });
+// });
